4cdd7b81308ad4e376da6544ba19b53a117460e8
Wallinger Marc <zadolux@inscomers.net>
2018-12-03 12:46:02 +0100

    Added main to trim_str

---
+++ trim_str.cpp
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------
+*				HTBLA-Leonding / Klasse: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 9
+ * Title:			trim.cpp
+ * Author:			M. Wallinger
+ * Due Date:		December 4, 2018
+ * ----------------------------------------------------------
+ * Description:
+ * Test functions for trim.h
+ * ----------------------------------------------------------
+ */
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
